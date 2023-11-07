e38930deec6d7dc8e35a27f833b9f9bf00e4ebd2
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2023-11-08 00:48:44 +0600

    updated

--- resources/views/livewire/my-qrcode/create.blade.php
+++ resources/views/livewire/my-qrcode/create.blade.php
@@ -249,7 +249,6 @@
                         </span>
                     </a>
 
-                    <x-button dark label="Dark" />
 
                     <a href="javascript:void(0)" wire:click="save()"
                         class="flex items-center justify-center bg-gradient-to-br rounded-lg p-2   from-gray-200 to-gray-200 hover:from-gray-300 hover:to-gray-300 dark:from-gray-700 dark:to-gray-700 dark:hover:from-gray-600 dark:hover:to-gray-600 text-neutral-900 dark:text-neutral-100">
