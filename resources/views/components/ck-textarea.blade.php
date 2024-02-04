f7b3a1bc46602929a67d01a9784932d0cc9306e4
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2024-02-04 21:03:54 +0600

    updated

--- resources/views/components/ck-textarea.blade.php
+++ resources/views/components/ck-textarea.blade.php
@@ -7,13 +7,52 @@
 
 
 @pushOnce('scripts')
+    <script src='https://cdn.ckeditor.com/4.15.1/full-all/ckeditor.js'></script>
     <script>
-            const editor = CKEDITOR.replace('{{ $id }}', {
-                filebrowserUploadUrl: "{{ route('ckeditor.image-upload', ['_token' => csrf_token()]) }}",
-                filebrowserUploadMethod: 'form',
-            });
-            editor.on('change', function(event) {
-                @this.set('{{ $id }}', event.editor.getData());
-            });
+
+        CKEDITOR.config.extraPlugins = 'codesnippet,uploadimage';
+        // CKEDITOR.config.codeSnippet_theme = 'monokai_sublime';
+        CKEDITOR.config.codeSnippet_languages = {
+            javascript: 'JavaScript',
+            php: 'PHP',
+            html: 'HTML',
+            css: 'CSS'
+        };
+        //toolbar
+
+        CKEDITOR.config.toolbar = [
+            { name: 'document', items: ['Source', '-', 'NewPage', 'Preview', '-', 'Templates'] },
+            { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+
+            { name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'] },
+            { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'PageBreak'] },
+            '/',
+            { name: 'styles', items: ['Styles', 'Format'] },
+            { name: 'justify', items: ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']},
+
+            { name: 'basicstyles', items: ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat'] },
+            { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote'] },
+            { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+            { name: 'tools', items: ['Maximize'] },
+            { name: 'others', items: ['-'] },
+            { name: 'colors', items: ['TextColor', 'BGColor']},
+            { name: 'custom', items: ['CodeSnippet', 'UploadImage'] },
+
+        ];
+
+
+        const editor = CKEDITOR.replace('{{ $id }}', {
+            filebrowserUploadUrl: "{{ route('ckeditor.image-upload', ['_token' => csrf_token()]) }}",
+            filebrowserUploadMethod: 'form',
+        });
+        editor.on('change', function(event) {
+            @this.set('{{ $id }}', event.editor.getData());
+        });
+
+
+
+
+
+
     </script>
 @endpushOnce
