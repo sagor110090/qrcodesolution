3077163808a1e24fed8063f7951c2c495e89fcb7
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2024-02-04 17:33:14 +0600

    updated

--- resources/views/pages/guest/auth/register.blade.php
+++ resources/views/pages/guest/auth/register.blade.php
@@ -49,10 +49,10 @@ $register = function(){
                 <x-ui.logo class="w-auto h-10 mx-auto text-gray-700 fill-current dark:text-gray-100" />
             </x-ui.link>
             <h2 class="mt-5 text-2xl font-extrabold leading-9 text-center text-gray-800 dark:text-gray-200">Create a new account</h2>
-            {{-- <div class="text-sm leading-5 text-center text-gray-600 dark:text-gray-400 space-x-0.5">
+            <div class="text-sm leading-5 text-center text-gray-600 dark:text-gray-400 space-x-0.5">
                 <span>Or</span>
                 <x-ui.text-link href="{{ route('login') }}">sign in to your account</x-ui.text-link>
-            </div> --}}
+            </div>
         </div>
 
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
@@ -66,10 +66,10 @@ $register = function(){
                             <x-ui.input label="Confirm Password" type="password" id="password_confirmation" name="password_confirmation" wire:model="passwordConfirmation" />
                             <x-ui.button type="primary" rounded="md" submit="true">Register</x-ui.button>
                         </form>
-                        <x-ui.button type="primary" rounded="md" submit="false" tag="a"
+                        {{-- <x-ui.button type="primary" rounded="md" submit="false" tag="a"
                                 href="{{ route('login.social', 'google') }}" style="margin-top: 10px;background-color: #dd4b39;border-color: #dd4b39;">
                                 Sign in with Google
-                            </x-ui.button>
+                            </x-ui.button> --}}
                     </div>
                 @endvolt
             </div>
