585432c67b01a0366f4c8483dc4eab7f3c24cf49
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2024-02-29 01:44:59 +0600

    updated

--- resources/views/dynamic/event-preview.blade.php
+++ resources/views/dynamic/event-preview.blade.php
@@ -41,7 +41,7 @@
                             <p class="text-base text-gray-700 md:text-lg"
                                 style="color:{{ Support::visibleColor($event->color) }}; font-family: {{ $event->font }};font-size:20px;">
                             <div class="flex items-center justify-center"
-                                style="color:{{ Support::visibleColor($event->color) }}; font-family: {{ $event->font }};font-size:20px;">
+                                style="color:{{ Support::visibleColor($event->color) }}; font-family: {{ $event->font }};font-size:20px;display: contents;">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round"
